25a3e174875aaaec20527c5d275c1a9724685ed1
Armand Grundmanis <grundmanweb@gmail.com>
2017-05-28 00:19:59 +0300

    upd

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,15 +1,15 @@
 var showAdvertSelect = document.getElementById('showAdverts');
 if (showAdvertSelect) {
     showAdvertSelect.onchange = function (e) {
-        var urlSplit = window.location.href.split('show/'),
+        var urlSplit = window.location.href.split('/show/'),
             currentUrl = urlSplit[0],
             getParams = '',
-            url = 'show/';
+            url = '/show/';
 
         if (urlSplit[1]) {
             getParams = urlSplit[1].split('?');
         }
 
-        window.location.href = currentUrl + url + this.value + (getParams[1] ? '?' + getParams : '');
+        window.location.href = currentUrl + url + this.value + (getParams[1] ? '?' + getParams[1] : '');
     };
 }
